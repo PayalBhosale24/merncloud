fd25e3098cd517aa032da83ecc10120e11f1e287
PayalBhosale24 <payalbhosale2507@gmail.com>
2024-12-06 16:12:35 +0530

    client database

--- config/db.js
+++ config/db.js
@@ -4,7 +4,7 @@ mongoose.set('strictQuery', true);
 
 const Connection = async () => {
     try {
-        await mongoose.connect("mongodb+srv://payalbhosale2507:Payal@1225@cluster0.ycn1i.mongodb.net/", { useNewUrlParser: true })
+        await mongoose.connect("mongodb+srv://payalbhosale2507:Payal%401225@cluster0.ycn1i.mongodb.net/", { useNewUrlParser: true })
         console.log(`Connected to Database Successfully`);
     } catch (error) {
         console.log('Disconnected', error);
